b1f7916ca689b56bf19b6c60a8a946d3526a1c2a
xShadeRoot <279212039+carlmoreau76-design@users.noreply.github.com>
2026-06-08 09:49:34 +0100

    Update index.js

--- api-4k/index.js
+++ api-4k/index.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const app = express();
 
+const images = [
+  "https://picsum.photos/800/1200",
+  "https://picsum.photos/801/1200",
+  "https://picsum.photos/802/1200"
+];
+
+app.get("/", (req, res) => {
+  res.send("API 4K is running 🚀");
+});
+
+app.get("/4k", (req, res) => {
+  const random = images[Math.floor(Math.random() * images.length)];
+  res.json({ url: random });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("API running on port " + PORT));
